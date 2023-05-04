9983277b93b1a4217fae9c2076a505a469da53ba
Matheus Peixoto <88630982+MatheusPxt21@users.noreply.github.com>
2023-05-04 19:35:10 -0300

    Teste fila pro Ready
    Modificações no .h e .c -> deu alguns erros, amanha tento arrumar

--- src/manager/readystate.h
+++ src/manager/readystate.h
@@ -1,24 +1,62 @@
+//
+// Created by matheuspeixoto on 04/05/23.
+//
+
 #ifndef SRC_READYSTATE_H
 #define SRC_READYSTATE_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
+/*
+typedef struct {
+    int id;
+    struct node* next;
+} node;
+typedef struct Ready *Apontador;
+typedef struct {
+    Apontador front;
+    Apontador rear;
+} queue;
+
+typedef struct Ready{
+    int size_at;
+    int maxSize;
+    queue queues[4];
+}Ready;
+*/
+typedef struct node{
+    int id;
+    struct node* next;
+}node;
+
+typedef struct Ready* Apontador;
 
 typedef struct {
-    int **queues; // An array of queues (arrays of integers)
-    int size; // The size of each queue
-} Ready;
+    Apontador front;
+    Apontador rear;
+} queue;
 
-void initReady(Ready *ready, int size);
 
-void insertReady(Ready *ready, int processId, int prior);
+typedef struct Ready{
+    int size_at, maxSize;
+    node No;
+    queue queues[4];
+}Ready;
 
-void removeReady(Ready *ready, int processId);
 
-int nextReady(Ready *ready);
 
+//inicializar
+//void initReady(Ready *ready, int size)
+void initReady(Ready *ready, int maxSize);
+queue* createQueue();
+void insertReady(Ready* rs,int id, int prior);
+//free
 void freeReady(Ready *ready);
-
+//pegar id do proximo
+int nextReady(Ready *ready);
+//remover
+void removeReady(Ready *ready, int processId);
+//print
 void printState( Ready *ready);
-
-void sortReady(Ready *ready);
 #endif
