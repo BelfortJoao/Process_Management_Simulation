7b329ed1899c4f4b495c10c41caf82530c45f334
matt <mateus.h.figueiredo@ufv.br>
2023-05-06 14:26:03 -0300

    Some comment docs

--- src/manager/cpu.h
+++ src/manager/cpu.h
@@ -1,23 +1,69 @@
-#ifndef SRC_CPU_H
-#define SRC_CPU_H
+/**
+ * @file cpu.h
+ * @brief Header file for the CPU module
+ */
 
-#include "timer.h"
+#ifndef CPU_H
+#define CPU_H
+
 #include "../process/process.h"
+#include "timer.h"
 
-typedef struct
+/**
+ * @struct CPU
+ * @brief Struct for the CPU module
+ */
+typedef struct CPU
 {
-    Process *runningProcess;
-    int programCounter;
-    Timer program_timer;
-    Timer executing_timer;
+    Process *runningProcess; /**< Pointer to the process that is currently running */
+    int programCounter; /**< Program counter */
+    Timer executing_timer; /**< Timer for the CPU's executing time */
+    Timer program_timer; /**< Timer for the current program's time */
 } CPU;
 
+/**
+ * @brief Initializes a new CPU with the program stored in the specified file
+ * @param filename Name of the file containing the program to load
+ * @return Pointer to the new CPU, or NULL if there was an error
+ */
 CPU *initializeCPU(char *filename);
 
-int interpreter(CPU *cpu, int *blk, char **file, int *PcPlus);
+/**
+ * @brief Converts a string to an integer
+ * @param string The string to convert
+ * @return The integer representation of the string, or 0 if the conversion failed
+ */
+int convertStringToInt(char *string);
 
+/**
+ * @brief Interprets a single line of the program
+ * @param cpu Pointer to the CPU that is executing the program
+ * @param blk Pointer to an integer that will be set to the ID of the block that the program should jump to, if applicable
+ * @param file Pointer to a string that will be set to the name of the file that the program should read from, if applicable
+ * @param PCPlus Pointer to an integer that will be set to the number of lines to skip after executing a "F" instruction, if applicable
+ * @return An integer indicating the next action to take, as follows:
+ *     - 0: Continue executing the program normally
+ *     - 1: Jump to a different block of the program
+ *     - 2: Terminate the program
+ *     - 3: Skip a specified number of lines in the program
+ *     - 4: Read input from a file
+ */
+int interpreter(CPU *cpu, int *blk, char **file, int *PCPlus);
+
+/**
+ * @brief Replaces the currently running process with a new process
+ * @param cpu Pointer to the CPU that is executing the program
+ * @param process Pointer to the new process
+ * @param programCounter New value for the program counter
+ * @param program_timer New value for the program timer
+ * @param executing_timer New value for the executing timer
+ */
 void changeProcess(CPU *cpu, Process *process, int programCounter, Timer program_timer, Timer executing_timer);
 
+/**
+ * @brief Frees the memory used by a CPU
+ * @param cpu Pointer to the CPU to free
+ */
 void freeCPU(CPU *cpu);
 
-#endif
+#endif /* CPU_H */
--- src/manager/processmanager.h
+++ src/manager/processmanager.h
@@ -1,44 +1,144 @@
+/**
+ * @file processmanager.h
+ * @brief Header file containing function declarations and struct definitions for the ProcessManager module.
+ */
+
 #ifndef SRC_PROCESSMANAGER_H
 #define SRC_PROCESSMANAGER_H
 
 #include "cpu.h"
 #include "processtable.h"
 
-typedef struct
+
+/**
+ * @brief Struct containing the necessary data for a Process Manager.
+ */
+typedef struct ProcessManager
 {
-    CPU *cpu; //Representa o unico núcleo que tem no computador
-    ProcessTable *processTable; //Tabela de processos do computador
-    Timer timer; //Tempo que o computador está ativo
-    int kill; //Indica se deve terminar o computador
-    int freeID; //Indica qualq é o proximo idArray livre para se criar um novo processo
+    CPU *cpu; /**< Pointer to the CPU struct. */
+    Timer timer; /**< Struct containing the current time. */
+    int freeID; /**< The next available process ID. */
+    int kill; /**< Flag indicating if the manager should stop execution. */
+    ProcessTable *processTable; /**< Pointer to the Process Table struct. */
 } ProcessManager;
 
+
+/**
+ * @brief Allocates memory for a Process Manager struct and initializes its members.
+ *
+ * @return A pointer to the Process Manager struct if successful, NULL otherwise.
+ */
 ProcessManager *initializeProcessManager();
 
+
+/**
+ * @brief Initializes a Process Manager with the given filename.
+ *
+ * @param filename The name of the file containing the CPU and process information.
+ * @return A pointer to the Process Manager struct if successful, NULL otherwise.
+ */
 ProcessManager *initializeProcessManagerFromFile(char *filename);
 
-void processExecuting(ProcessManager *processManager);
 
-void scheduleProcess(ProcessManager *processManager);
-
-void clockUpPC(ProcessManager *processManager);
-
-void freeProcessManager(ProcessManager *processManager);
-
-void upperInterpreter(ProcessManager *processManager);
-
+/**
+ * @brief Blocks a process for a given amount of time.
+ *
+ * @param processManager Pointer to the Process Manager struct.
+ * @param blockTime The amount of time to block the process for.
+ */
 void blockProcess(ProcessManager *processManager, int blockTime);
 
-void processUnblock(ProcessManager *processManager);
 
-void processRewind(ProcessManager *processManager, char *filename);
+/**
+ * @brief Schedules a process to run on the CPU.
+ *
+ * @param processManager Pointer to the process manager.
+ */
+void scheduleProcess(ProcessManager *processManager);
 
+
+/**
+ * @brief Ends the currently executing process.
+ *
+ * @param processManager Pointer to the process manager.
+ */
 void endProcess(ProcessManager *processManager);
 
-void processCP(ProcessManager *processManager, Process *process, int PcPlus);
 
+/**
+ * @brief Executes the next process on the ready queue.
+ *
+ * @param processManager Pointer to the process manager.
+ */
 void execute(ProcessManager *processManager);
 
+
+/**
+ * @brief Continues executing the current process if the CPU is not empty,
+ * or executes the next process on the ready queue if it is.
+ *
+ * @param processManager Pointer to the process manager.
+ */
+void processExecuting(ProcessManager *processManager);
+
+
+/**
+ * @brief Removes blocked processes that are ready to run and inserts them into the ready queue.
+ *
+ * @param processManager Pointer to the process manager struct.
+ */
+void processUnblock(ProcessManager *processManager);
+
+
+/**
+ * @brief Advances the clock and blocks processes that exceeded their time quantum.
+ *
+ * @param processManager Pointer to the process manager struct.
+ */
+void clockUpPC(ProcessManager *processManager);
+
+
+/**
+ * @brief Creates a copy of the current process and inserts it into the process table.
+ *
+ * @param processManager Pointer to the process manager struct.
+ * @param process Pointer to the process struct.
+ * @param PcPlus Integer value of program counter.
+ */
+void processCP(ProcessManager *processManager, Process *process, int PcPlus);
+
+
+/**
+ * @brief Rewinds the current process from the given file and inserts it into the process table.
+ *
+ * @param processManager Pointer to the process manager struct.
+ * @param filename Pointer to the name of the file to rewind the process from.
+ */
+void processRewind(ProcessManager *processManager, char *filename);
+
+
+/**
+ * @brief Updates the process table with the execution information of the current process.
+ *
+ * @param processManager Pointer to the process manager struct.
+ */
 void attExec(ProcessManager *processManager);
+
+
+/**
+ * @brief Interprets the instruction of the current process and executes the appropriate action.
+ *
+ * @param processManager Pointer to the process manager struct.
+ */
+void upperInterpreter(ProcessManager *processManager);
+
+
+/**
+ * @brief Frees the allocated memory for the process manager struct and its elements.
+ *
+ * @param processManager Pointer to the process manager struct.
+ */
+void freeProcessManager(ProcessManager *processManager);
+
 
 #endif
