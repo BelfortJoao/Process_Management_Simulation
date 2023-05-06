652c01584155cc13bc2ca33f1d3f5d2dfb24567e
matt <mateus.h.figueiredo@ufv.br>
2023-05-06 14:26:03 -0300

    Some comment docs

--- src/control/control.h
+++ src/control/control.h
@@ -1,17 +1,37 @@
-#ifndef SRC_CONTROL_H
-#define SRC_CONTROL_H
+/**
+ * @file control.h
+ * @brief Header file for the Control module.
+ */
+
+#ifndef CONTROL_H
+#define CONTROL_H
 
 #include "../manager/processmanager.h"
 #include "../printer/printer.h"
 
-typedef struct
+/**
+ * @struct Control
+ * @brief Struct that holds pointers to the process manager and printer.
+ */
+typedef struct Control
 {
-    ProcessManager *processManager;
-    Printer *printer;
+    ProcessManager *processManager; /**< Pointer to the process manager. */
+    Printer *printer; /**< Pointer to the printer. */
 } Control;
 
+/**
+ * @brief Allocates memory for a new Control struct and initializes its process manager and printer.
+ *
+ * @return Pointer to the newly created Control struct, or NULL if memory allocation fails.
+ */
 Control *initializeControl();
 
+/**
+ * @brief Runs the control loop for the simulation.
+ *
+ * @param control Pointer to the Control struct.
+ * @return 0 if successful, -1 if an error occurs.
+ */
 int runControl(Control *control);
 
-#endif
+#endif /* CONTROL_H */
