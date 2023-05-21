dd854788c489bc4539e545c5395de5ae6d722c8d
matt <mateus.h.figueiredo@ufv.br>
2023-05-20 23:12:40 -0300

    Fix print

--- src/control/control.c
+++ src/control/control.c
@@ -77,8 +77,9 @@ int runControl(Control *control)
 
     // Cria um pipe para comunicação entre pai e filho
     int fd[2];
+    int fdFlag[2];
 
-    if (pipe(fd) == ERROR)
+    if (pipe(fd) == ERROR || pipe(fdFlag) == ERROR)
     {
         printf("Error: pipe failed\n");
         return ERROR;
@@ -99,9 +100,12 @@ int runControl(Control *control)
 
         while (read(fd[0], &command, 1) > 0)
         {
+            write(fdFlag[1], "R", 1);
+
             if (command == 'u') // Se receber um 'u' do pipe, executa o processo
             {
                 processExecuting(control->processManager);
+                write(fdFlag[1], "F", 1);
             }
             else if (command == 'i') // Se receber um 'i' do pipe, cria um processo e imprime "PRINT"
             {
@@ -117,6 +121,7 @@ int runControl(Control *control)
                     printProcessTable(control->processManager->processTable);
                     printState(control->processManager->processTable->ready);
                     printBlocked(control->processManager->processTable->blockedQueue);
+                    write(fdFlag[1], "F", 1);
                 }
                 else if (newPid == PARENT)
                 {
@@ -135,6 +140,7 @@ int runControl(Control *control)
                 else if (newPid == CHILD)
                 {
                     printAverageResponseTime(calcAverageResponseTime(control->processManager->artCounter));
+                    write(fdFlag[1], "F", 1);
                     return 0;
                 }
                 else // Processo pai
@@ -200,18 +206,29 @@ int runControl(Control *control)
                         printf(INVALID_COMMAND, character);
                         break;
                 }
+
+                close(fdFlag[1]);
+                char comm = 'R';
+
+                while (comm != 'F')
+                {
+                    read(fdFlag[0], &comm, 1);
+                }
+
+                close(fdFlag[0]);
             }
 
             fclose(inputFile);
         }
         else // Se o input for por linha de comando
         {
+            cleanStdin();
+
             char command;
-
-            printf("Type the commands: :)");
-
             while (true)
             {
+                printf("\nType the commands: ");
+
                 while ((command = (char) getchar()) != EOF && command != '\n' && command != '\r')
                 {
                     switch (toupper(command))
@@ -230,6 +247,13 @@ int runControl(Control *control)
                         default:
                             printf(INVALID_COMMAND, command);
                             break;
+                    }
+
+                    char comm = 'R';
+
+                    while (comm != 'F')
+                    {
+                        read(fdFlag[0], &comm, 1);
                     }
                 }
             }
