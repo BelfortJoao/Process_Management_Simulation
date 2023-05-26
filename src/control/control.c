4a44b553804460b3fc4ba85e5793402177b5de48
Matt <mateush.study@gmail.com>
2023-05-26 08:39:41 -0300

    Revert "I guess it's working now"
    This reverts commit a125a3afc84a8310dbeb70ee6e2367f88c6cc7cc.

--- src/control/control.c
+++ src/control/control.c
@@ -207,12 +207,15 @@ int runControl(Control *control)
                         break;
                 }
 
+                close(fdFlag[1]);
                 char comm = 'R';
 
                 while (comm != 'F')
                 {
                     read(fdFlag[0], &comm, 1);
                 }
+
+                close(fdFlag[0]);
             }
 
             fclose(inputFile);
